f89ecdbb22a51021270147d99429373a9ed5ebec
jonmolinna <jmolina2624@gmail.com>
2020-09-10 08:20:18 -0500

    Componentes

---
+++ mern-stack-6/client/src/components/Transaction.js
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { numberWithCommas } from '../utils/format';
+
+const Transaction = (props) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+
+    const sign = props.transaction.amount < 0 ? '-' : '+';
+
+    return (
+        <li className={props.transaction.amount < 0 ? 'minus' : 'plus'}>
+            {props.transaction.text} <span>{sign}${numberWithCommas(Math.abs(props.transaction.amount))}</span>
+            <button 
+                className="delete-btn"
+                onClick={() => deleteTransaction(props.transaction._id)}
+            >
+                x
+            </button>
+        </li>
+    )
+}
+
+export default Transaction;
